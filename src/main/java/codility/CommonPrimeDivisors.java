77c3a8209cf441660e9e1a98e0f413e413cc0c11
조소현 <s2chuu@humuson.com>
2022-08-09 17:12:32 +0900

    최대공약수 구하는 함수 수정

--- src/main/java/codility/CommonPrimeDivisors.java
+++ src/main/java/codility/CommonPrimeDivisors.java
@@ -25,27 +25,18 @@ public class CommonPrimeDivisors {
     if (y == 1) {
       return true;
     }
-    int gcd = gcd(x, y, 1);
+    int gcd = gcd(x, y);
     if (gcd == 1) {
       return false;
     }
     return hasAllPrimeFactors(x, y/gcd);
   }
 
-  public int gcd(int a, int b, int res) {
-    if (a == b) {
-      return res * a;
-    } else if ((a % 2 == 0) && (b % 2 == 0)) {
-      return gcd(a/2, b/2, 2*res);
-    } else if (a % 2 == 0) {
-      return gcd(a/2, b, res);
-    } else if (b % 2 == 0) {
-      return gcd(a, b/2, res);
-    } else if (a > b) {
-      return gcd(a-b, b, res);
-    } else {
-      return gcd(a, b-a, res);
+  public int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
     }
+    return gcd(b, a%b);
   }
 
   public static void main(String[] args) {
